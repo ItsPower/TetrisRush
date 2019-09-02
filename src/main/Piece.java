b4b80b4e5593bd7cff8c1987f77ec044ef72cec8
Antoine KOZLOV <antoine.kozlov.etu@univ-lille.fr>
2019-09-02 14:59:29 +0200

    Ajout position

--- src/main/Piece.java
+++ src/main/Piece.java
@@ -2,11 +2,16 @@ package main;
 
 public class Piece {
 	
-	Orientation orientation = Orientation.BAS;
-	Forme forme;
-
-	public Piece() {
-		
+	private Orientation orientation = Orientation.BAS;
+	private Forme forme;
+	private Position positionPrincipale = new Position(0,0); //Fixe au centre du plateau
+	
+	public Piece(Forme forme) {
+		this.forme = forme;
+	}
+	
+	public boolean rotation() {
+		return false;
 	}
 	
 }
