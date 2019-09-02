3753026e239aacc6bb7742a30b9b1002b072c4fa
Antoine KOZLOV <antoine.kozlov.etu@univ-lille.fr>
2019-09-02 15:28:15 +0200

    Position relative

--- src/main/Forme.java
+++ src/main/Forme.java
@@ -2,6 +2,57 @@ package main;
 
 public enum Forme {
 	
-	CARRE, LIGNE, ESCD, ESCG, T, LG, LD;
+	CARRE(new Position[] {
+			new Position(0,0),
+			new Position(1,0),
+			new Position(-1,1),
+			new Position(0,-1)
+	}),
+	LIGNE(new Position[] {
+			new Position(0,0),
+			new Position(0,1),
+			new Position(0,2),
+			new Position(0,3)
+	}),
+	ESCD(new Position[] {
+			new Position(0,0),
+			new Position(1,0),
+			new Position(-1,-1),
+			new Position(0,-1)
+	}),
+	ESCG(new Position[] {
+			new Position(0,0),
+			new Position(-1,0),
+			new Position(0,1),
+			new Position(1,-1)
+	}),
+	T(new Position[] {
+			new Position(0,0),
+			new Position(-1,0),
+			new Position(1,0),
+			new Position(0,-1)
+	}),
+	LG(new Position[] {
+			new Position(0,0),
+			new Position(0,1),
+			new Position(0,2),
+			new Position(-1,2)
+	}),
+	LD(new Position[] {
+			new Position(0,0),
+			new Position(0,1),
+			new Position(0,2),
+			new Position(1,2)
+	});
 
+	private Position[] positionRelative;
+	
+	private Forme(Position[] positionRelative) {
+		this.positionRelative = positionRelative;
+	}
+	
+	public Position[] getPositionRelative() {
+		return positionRelative;
+	}
+	
 }
--- src/main/Piece.java
+++ src/main/Piece.java
@@ -4,13 +4,19 @@ public class Piece {
 	
 	private Orientation orientation = Orientation.BAS;
 	private Forme forme;
-	private Position positionPrincipale = new Position(0,0); //Fixe au centre du plateau
+	private Position positionPlateau = new Position(2,0);
+	private Position positionRelative[] = new Position[4]; //positionRelative[0] = position principale de la piece
 	
 	public Piece(Forme forme) {
 		this.forme = forme;
+		positionRelative = forme.getPositionRelative();
 	}
 	
 	public boolean rotation() {
+		return false;
+	}
+	
+	public boolean translation() {
 		return false;
 	}
 	
