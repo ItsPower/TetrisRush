77483d37da80aa096859b0f79088e13c2d391961
Théo Petit <theo.petit.etu@univ-lille.fr>
2019-09-03 14:41:59 +0200

    Rotation fonctionnelle

--- src/main/Piece.java
+++ src/main/Piece.java
@@ -18,6 +18,7 @@ public class Piece {
 	public Position[] rotationG() {
 		Position[] positionTemp = new Position[4];
 		for(int i = 0; i < 4; i++) {
+			Main.getInstance().p.getPlateau()[positionPlateau.getX() + positionRelative[i].getX()][positionPlateau.getY() + positionRelative[i].getY()] = false;
 			anciennePositionRelative[i] = new Position(positionRelative[i]);
 			positionRelative[i].setPosition(positionRelative[i].getY(), positionRelative[i].getX()*-1);
 			positionTemp[i] = new Position(positionRelative[i].getY(), positionRelative[i].getX()*-1);
@@ -28,6 +29,7 @@ public class Piece {
 	public Position[] rotationD() {
 		Position[] positionTemp = new Position[4];
 		for(int i = 0; i < 4; i++) {
+			Main.getInstance().p.getPlateau()[positionPlateau.getX() + positionRelative[i].getX()][positionPlateau.getY() + positionRelative[i].getY()] = false;
 			anciennePositionRelative[i] = new Position(positionRelative[i]);
 			positionRelative[i].setPosition(positionRelative[i].getY()*-1, positionRelative[i].getX());
 			positionTemp[i] = new Position(positionRelative[i].getY()*-1, positionRelative[i].getX());
