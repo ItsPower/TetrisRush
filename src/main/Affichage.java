c891a4e905383eb5fbf64daf4ee66536ee513c22
Tronel <tronel70@gmail.com>
2019-09-03 08:48:41 +0200

    Charles

--- src/main/Affichage.java
+++ src/main/Affichage.java
@@ -1,13 +1,11 @@
 package main;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
 
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
-public class Affichage implements KeyListener {
+public class Affichage {
 
 	private static Affichage instance;
 	
@@ -18,7 +16,7 @@ public class Affichage implements KeyListener {
 
 	public Affichage() {
 		Terminal term;
-		try { 
+		try {
 			term = TerminalBuilder.terminal();
 			term_height = term.getHeight();
 			term_width = term.getWidth();
@@ -27,34 +25,34 @@ public class Affichage implements KeyListener {
 		}
 		clear();
 		String titre = "TETRIS S3 - Projet AGILE";
-		curseur(3, term_width/2-titre.length()/2);
+		curseur(3, term_width / 2 - titre.length() / 2);
 		syso(titre);
-		
-		base_x = term_width/2-PLATEAU_WIDTH/2;
+
+		base_x = term_width / 2 - PLATEAU_WIDTH / 2;
 		base_y = 8;
-		
-		for(int x=base_x-4; x<base_x+PLATEAU_WIDTH+4; x++)
-			for(int y=6; y<PLATEAU_HEIGHT+10; y++) {
-				curseur(y, x);
-				syso("█");
+
+		for (int x = base_x - 4; x < base_x + PLATEAU_WIDTH + 4; x++) {
+			for (int y = 6; y < PLATEAU_HEIGHT + 10; y++) {
+				curseur(y, x); syso("█");
 			}
-		for(int x=base_x; x<base_x+PLATEAU_WIDTH; x++)
-			for(int y=base_y; y<PLATEAU_HEIGHT+base_y; y++) {
-				curseur(y, x);
-				syso(" ");
+		}
+
+		for(int x=base_x; x<base_x+PLATEAU_WIDTH; x++) {
+			for (int y = base_y; y < PLATEAU_HEIGHT + base_y; y++) {
+				curseur(y, x); syso(" ");
 			}
+		}
 	}
 	
 	public void printPiece(Piece p) {
 		Position base = p.getPositionPlateau();
 		Position[] posis = p.getPositionRelative();
-		for(Position tmp : posis) {
+
+		for(Position tmp : posis)
 			printCube(new Position(base.getX() + tmp.getX(), base.getY() + tmp.getY()));
-		}
 	}
 
 	private void printCube(Position pos) { //TODO COULEURS TO ADD
-		
 		curseur(base_y + pos.getY()*2, base_x + pos.getX()*4);
 		syso("\033[91m████\033[37m");
 		
@@ -62,39 +60,16 @@ public class Affichage implements KeyListener {
 		syso("\033[91m████\033[37m");
 	}
 	
-	public void syso(String s) {
-		System.out.print(s);
-	}
+	public void syso(String s) {System.out.print(s);}
 
-	public void curseur(int lig, int col) {
-		syso("\033[" + lig + ";" + col + "H");
-	}
+	public void curseur(int lig, int col) {syso("\033[" + lig + ";" + col + "H");}
 	
-	public void clear() {
-		syso("\033[2J");
-	}
+	public void clear() {syso("\033[2J");}
 
 	public static Affichage getInstance() {
-		if(instance == null) {
+		if(instance == null)
 			instance = new Affichage();
-		}
+
 		return instance;
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		syso("COUCOU");
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		syso("COUCOU");
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
